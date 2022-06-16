290c2a4c28ced7ff542a1543b01f341e88890352
mertonwagner <107504603+mertonwagner@users.noreply.github.com>
2022-06-16 14:14:45 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+let on = 0
+let dogonMat = 0
+let DogatDoor = 0
+input.onButtonPressed(Button.A, function () {
+    on = 1
+})
+input.onButtonPressed(Button.B, function () {
+    on = 0
+})
+basic.forever(function () {
+    while (on == 1) {
+        dogonMat = pins.analogReadPin(AnalogPin.P0)
+        basic.showNumber(pins.analogReadPin(AnalogPin.P0))
+        DogatDoor = 0
+        if (dogonMat > 500) {
+            DogatDoor += 1
+            for (let index = 0; index < 2; index++) {
+                music.playTone(262, music.beat(BeatFraction.Double))
+                basic.showLeds(`
+                    # . . . .
+                    # . . # #
+                    # # # # #
+                    . # # # .
+                    # . . . #
+                    `)
+                basic.pause(1000)
+                basic.clearScreen()
+            }
+        } else {
+        	
+        }
+    }
+})
